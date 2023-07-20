3ac883ffff539f552bf02e28ac8222779bc95524
mahmood-h01 <93209526+mahmood-h01@users.noreply.github.com>
2023-07-20 14:21:11 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xc4624e065c1c47f0dd0670408aabf7f565389293";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
